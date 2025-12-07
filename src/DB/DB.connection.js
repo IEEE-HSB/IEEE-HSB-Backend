4309757c25892edf082e9803742ed6f6330c39a6
Mohammed Shamel <160044480+Mohamed-shamel1@users.noreply.github.com>
2025-12-07 20:27:54 +0200

    fix

--- src/DB/DB.connection.js
+++ src/DB/DB.connection.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
     try {
-       const result = await mongoose.connect(process.env.MONGOOSE_URL||'mongodb://localhost:27017/IEEE' , {
+        console.log("Connecting to database...");
+        console.log(process.env.MONGOOSE_URL);
+        
+       const result = await mongoose.connect(process.env.MONGOOSE_URL , {
         serverSelectionTimeoutMS: 5000, //ديه عشان بتعمل تايماوت للاتصال بالداتا بيز
        });
         console.log("Database connected");
